b43634d64be3361b139128702c078e4a50b77a98
Oleg <byosipow.second@gmail.com>
2025-03-30 14:52:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -22,21 +22,20 @@ int countPairs2(int *arr, int len, int value) {
                 int n = right - left + 1;
                 count += n * (n - 1) / 2;
                 break;
-            } else {
-                int left_val = arr[left];
-                int right_val = arr[right];
-                int left_count = 0;
-                int right_count = 0;
-                while (left < len && arr[left] == left_val) {
-                    left++;
-                    left_count++;
-                }
-                while (right >= 0 && arr[right] == right_val) {
-                    right--;
-                    right_count++;
-                }
-                count += left_count * right_count;
             }
+            int left_val = arr[left];
+            int right_val = arr[right];
+            int left_count = 0;
+            while (arr[left] == left_val) {
+                left++;
+                left_count++;
+            }
+            int right_count = 0;
+            while (arr[right] == right_val) {
+                right--;
+                right_count++;
+            }
+            count += left_count * right_count;
         } else if (sum < value) {
             left++;
         } else {
@@ -46,36 +45,31 @@ int countPairs2(int *arr, int len, int value) {
     return count;
 }
 
-int binarySearch(int *arr, int left, int right, int value) {
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (arr[mid] == value) {
-            return mid;
-        } else if (arr[mid] < value) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
-    return -1;
-}
-
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len; ++i) {
         int complement = value - arr[i];
-        int idx = binarySearch(arr, i + 1, len - 1, complement);
-        if (idx != -1) {
-            count++;
-            int left = idx + 1;
-            while (left < len && arr[left] == complement) {
+        int low = i + 1;
+        int high = len - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == complement) {
                 count++;
-                left++;
-            }
-            int right = idx - 1;
-            while (right > i && arr[right] == complement) {
-                count++;
-                right--;
+                int left = mid - 1;
+                while (left >= low && arr[left] == complement) {
+                    count++;
+                    left--;
+                }
+                int right = mid + 1;
+                while (right <= high && arr[right] == complement) {
+                    count++;
+                    right++;
+                }
+                break;
+            } else if (arr[mid] < complement) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
     }
