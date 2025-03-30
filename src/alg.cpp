746f47657745a373d845d96526c4af422e621589
Oleg <byosipow.second@gmail.com>
2025-03-30 15:15:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,7 @@
-#include <algorithm>
 // Copyright 2021 NNTU-CS
+#include <algorithm>
+#include <unordered_map>
+
 int countPairs1(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len; ++i) {
@@ -23,21 +25,13 @@ int countPairs2(int *arr, int len, int value) {
                 count += (right - left + 1) * (right - left) / 2;
                 break;
             }
-            int left_val = arr[left];
-            int right_val = arr[right];
-            int left_count = 1;
-            while (left + 1 < len && arr[left + 1] == left_val) {
-                left++;
-                left_count++;
-            }
-            int right_count = 1;
-            while (right - 1 >= 0 && arr[right - 1] == right_val) {
-                right--;
-                right_count++;
-            }
-            count += left_count * right_count;
-            left++;
-            right--;
+            int temp_left = left;
+            int temp_right = right;
+            while (arr[temp_left] == arr[left]) temp_left++;
+            while (arr[temp_right] == arr[right]) temp_right--;
+            count += (temp_left - left) * (right - temp_right);
+            left = temp_left;
+            right = temp_right;
         } else if (sum < value) {
             left++;
         } else {
@@ -48,37 +42,14 @@ int countPairs2(int *arr, int len, int value) {
 }
 
 int countPairs3(int *arr, int len, int value) {
+    std::unordered_map<int, int> freq;
     int count = 0;
-    std::sort(arr, arr + len);
-    
     for (int i = 0; i < len; ++i) {
         int complement = value - arr[i];
-        int left = i + 1;
-        int right = len - 1;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            if (arr[mid] == complement) {
-                count++;
-                int l = mid - 1;
-                while (l >= left && arr[l] == complement) {
-                    count++;
-                    l--;
-                }
-                
-                int r = mid + 1;
-                while (r <= right && arr[r] == complement) {
-                    count++;
-                    r++;
-                }
-                break;
-            } else if (arr[mid] < complement) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+        if (freq.find(complement) != freq.end()) {
+            count += freq[complement];
         }
+        freq[arr[i]]++;
     }
     return count;
 }
